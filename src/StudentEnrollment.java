8fbe01b8c50cac1e0eb6425bba2f3f8e6a8fc0a0
jose jimenez <jimenjos000@warren.k12.in.us>
2024-11-08 13:08:37 -0500

    vbcf

---
+++ src/StudentEnrollment.java
@@ -0,0 +1,13 @@
+public class StudentEnrollment {
+    public static void main(String[] args) {
+        Student student1 = new Student("12345", "John", "Doe", 20, "Computer Science");
+        Student student2 = new Student("23456", "Jane", "Smith", 21, "Mathematics");
+        Student student3 = new Student("34567", "Bob", "Johnson", 19, "Physics");
+        System.out.println(student1);
+        System.out.println(student2);
+        System.out.println(student3);
+
+
+    }
+}
+
